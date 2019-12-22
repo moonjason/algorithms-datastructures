1507df2a9afa98be36fa2fa7921dab0a8987e9a3
Jason Moon <jasonmoonjm@gmail.com>
2019-12-21 23:44:12 -0800

    big o simplification start

---
+++ bigO/simplifyingBigO.js
@@ -0,0 +1,17 @@
+// simplify these big O expressions as much as possible
+
+// 1) O(n + 10)
+// => O(n)
+
+// 2) O(100 * n)
+// => O(n)
+
+// 3) O(25)
+// => 0(1)
+
+// 4) O(n^2 + n^3)
+// =>  O(n^3)
+
+// 5) O(n + n + n + n)
+// => O(n)
+
