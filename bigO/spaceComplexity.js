f81f9409475e62de02dfe0ad51432d0185e11d51
Jason Moon <jasonmoonjm@gmail.com>
2020-01-17 14:00:53 -0800

    space complex setup

---
+++ bigO/spaceComplexity.js
@@ -0,0 +1,9 @@
+//Determine the space complexity for the following function  
+
+// function logUpTo(n) {
+//     for (var i = 1; i <= n; i++) {
+//         console.log(i);
+//     }
+// }
+
+// => O(1)
