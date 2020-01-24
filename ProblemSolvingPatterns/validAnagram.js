1166f41916297c927c4c98b799af762ba64ccd05
moonjason <jasonmoonjm@gmail.com>
2020-01-24 13:39:56 -0800

    add new solution valid anagram

--- ProblemSolvingPatterns/validAnagram.js
+++ ProblemSolvingPatterns/validAnagram.js
@@ -24,3 +24,30 @@ const validAnagram = (str1, str2) => {
     return (map.size === 0 ? true : false)
 }
 
+// second solution
+
+const validAnagram = (str1, str2) => {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < str1.length; i++) {
+        let letter = first[i] // this doesn't require splitting the string...
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    for (let i = 0; i < str2.length; i++) {
+        let letter = str2[i]
+        // cant find letter or letter is zero than it's not an anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+
+    return true;
+}
