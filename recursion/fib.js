2bc0bb2209038311e172ba9c8827219718daf993
Jason Moon <jasonmoonjm@gmail.com>
2020-04-28 20:48:10 -0700

    update fib

--- recursion/fib.js
+++ recursion/fib.js
@@ -1,9 +1,9 @@
 // recursive fib accepts a nubmer and returns the nth number in the fib sequence 
 
 const fib = (num) => {
-    if(num === 1) return 1;
+    if(num <= 2) return 1;
 
-    return num + fib(num -1);
+    return fib(num-2) + fib(num -1);
 }
 
 console.log(fib(10));
