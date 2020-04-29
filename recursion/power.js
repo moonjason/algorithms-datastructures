ae16deffe3701ab93e1a0fbec2a04d5a96511276
Jason Moon <jasonmoonjm@gmail.com>
2020-04-28 20:14:24 -0700

    power recursion 2

--- recursion/power.js
+++ recursion/power.js
@@ -7,11 +7,11 @@
 //power(2, 4) //16
 
 function power(num, exp){
-    if(exp === 1) {
-        return num;
+    if(exp === 0) {
+        return 1;
     }
 
-    return num *= power(num, exp - 1);
+    return num * power(num, exp - 1);
 }   
 
 console.log(power(2,4))
