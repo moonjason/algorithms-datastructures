4178177239ee2c92f9ca3537ef5a83c26621334c
Jason Moon <jasonmoonjm@gmail.com>
2020-05-01 19:19:01 -0700

    capitalize first

---
+++ recursionAdvanced/capitalizeFirst.js
@@ -0,0 +1,18 @@
+// function given an array of strings, capitalize the first letter of each string in the array
+
+const capitalizeFirst = (array) => {
+    if(array.length === 1) {
+        let newItem = array[0][0]
+        return [`${array[0][0].toUpperCase()}${array[0].substr(1)}`];
+    }
+
+    const res = capitalizeFirst(array.slice(0, -1));
+    const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length - 1)[0].substr(1);
+    res.push(string);
+    return res;
+}
+  
+// using slice and substring for arrays and strings 
+
+console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
+  
