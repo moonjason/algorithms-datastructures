70f3dfbae9c6f5196a255c873ec2161cb2d5b7a9
moonjason <jasonmoonjm@gmail.com>
2020-05-02 18:17:17 -0700

    binary search refined version plus notes

--- searchingAlgos/binarySearch.js
+++ searchingAlgos/binarySearch.js
@@ -25,4 +25,25 @@ const binarySearch = (array, val) => {
     return middle;
 }    
 
+// =======
+// REFINED VERSION 
+// =======
+
+// const binarySearch = () => {
+//     let start = 0;
+//     let end = array.length - 1;
+//     let middle = Math.floor((start + end ) / 2);
+
+//     while(array[middle] !== val && start <= end) {
+//         if(elem < arr[middle]) end = middle - 1;
+//         else start = middle + 1;
+//         middle = Math.floor((start + end) / 2);
+//     }
+//     return arr[middle] === elem ? middle : -1;
+// }
+
+
+// NOTES
+// the reason why our while loop case is if array doesn't equal to the value AND start is less than or equal to the end 
+// is because when we don't find a middle, we break the loop of the start reaches the end and the value was never found in the middle
 console.log(binarySearch([1,2,3,4,5], 3));
