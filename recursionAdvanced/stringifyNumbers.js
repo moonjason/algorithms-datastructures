151f1612f3c52860550e67f78f65d429374eb4fa
moonjason <jasonmoonjm@gmail.com>
2020-05-05 13:12:12 -0700

    notes for recursions and objects

--- recursionAdvanced/stringifyNumbers.js
+++ recursionAdvanced/stringifyNumbers.js
@@ -27,21 +27,29 @@ let obj = {
 }
 */
 
-const stringifyNumbers = (obj) => {
-    let res = {};
-    let objKeys = Object.keys(obj);
-
-    // if (typeof obj[objKeys[0]] === "object")
-
-    console.log(objKeys.slice(0, objKeys.length - 1));
-    // set result equal to object property 
-
-
-    //object assign  
-
-
-
-    return res;
-} 
+function stringifyNumbers(obj) {
+    var newObj = {};
+    for (var key in obj) {
+      if (typeof obj[key] === 'number') {
+        newObj[key] = obj[key].toString();
+      } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+        newObj[key] = stringifyNumbers(obj[key]);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+ }
 
 stringifyNumbers(obj);
+
+// assign empty object
+// we loop through the keys of our input object
+// if the key value is a number
+// we give our empty object the key and its value.toString();
+// if key is an object and not an array (cuz arrays are objects)
+// we set the key in our empty obj, and set it equal to recalling the function with the keys in the object
+// we recall here to make any adjustments to keys that are nested in one another
+// if the object key is a not a number or object we just return the key
+
+// WE ARE ONLY USING RECURSION HERE TO TRAVERSE THROUGH OUR KEYS THAT ARE OBJECTS 
