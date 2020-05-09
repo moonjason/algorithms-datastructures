727f7ed66b6b6001bde3e77f6938723ed550bdac
moonjason <jasonmoonjm@gmail.com>
2020-05-09 14:16:34 -0700

    digit count math.log10 notes

--- sortingAlgosAdvanced/digitCount.js
+++ sortingAlgosAdvanced/digitCount.js
@@ -5,4 +5,18 @@ const digitCount = (num) => {
     return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
-console.log(digitCount(423));
+
+
+// again math.abs is incase the number is negative
+// Math.log10  =  log 10 423  (423 being our input)
+// NOTE ABOUT LOGARITHMS:
+// If there is no base written in a log, assume its 10
+
+console.log(digitCount(423)); // 3
+
+// Math.log10(423)
+// 2.626340~
+// Math.floor(2.626340~)
+// 2 
+// 2 + 1
+// 3
