44ebdb709ada402f04cb96e84a7dc78b19a322ac
moonjason <jasonmoonjm@gmail.com>
2020-05-23 15:21:07 -0700

    recursion power stepbystep

--- recursion/power.js
+++ recursion/power.js
@@ -15,3 +15,17 @@ function power(num, exp){
 }   
 
 console.log(power(2,4))
+
+
+// function power(num, exp){
+//     if(exp === 0) {
+//         return 1;
+//     }
+
+//     return num * power(num, exp - 1);
+//              2 * power(2, 3)
+//                  2 * power(2, 2);    
+//                      2 * power(2, 1);
+//                           2 * power(2, 0);
+//                                  1
+// }                    
