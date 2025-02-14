7696ac1e3f3a1ca0d454a928f0a6160c42296601
moonjason <jasonmoonjm@gmail.com>
2025-02-13 21:26:32 -0800

    two sum

---
+++ python/hash_table/two_sum.py
@@ -0,0 +1,42 @@
+def two_sum(nums, target):
+    ht = {}
+    result = []
+    
+    for i,num in enumerate(nums):
+        if target - num in ht:
+            return [ ht[target - num], i ]
+        else:
+            ht[num] = i
+        
+    return result 
+
+# checks if the difference between the target and the number exists in the hash table   
+# stores every number
+    
+   
+print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+print(two_sum([1, 3, 5, 7, 9], 10))  
+print ( two_sum([1, 2, 3, 4, 5], 10) )
+print ( two_sum([1, 2, 3, 4, 5], 7) )
+print ( two_sum([1, 2, 3, 4, 5], 3) )
+print ( two_sum([], 0) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 4]
+    [1, 3]
+    [0, 3]
+    [1, 3]
+    []
+    [2, 3]
+    [0, 1]
+    []
+
+"""
+
+
