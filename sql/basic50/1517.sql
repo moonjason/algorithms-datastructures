945611d5647f8819ac13f1ed4246a2b548d5c1e3
moonjason <jasonmoonjm@gmail.com>
2025-04-04 15:28:25 -0700

    grind

---
+++ sql/basic50/1517.sql
@@ -0,0 +1,32 @@
+-- https://leetcode.com/problems/find-users-with-valid-e-mails/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Users
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- | mail          | varchar |
+-- +---------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+-- This table contains information of the users signed up in a website. Some e-mails are invalid.
+
+ 
+
+-- Write a solution to find the users who have valid emails.
+-- A valid e-mail has a prefix name and a domain where:
+
+--     The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+--     The domain is '@leetcode.com'.
+-- Return the result table in any order.
+
+
+
+SELECT * FROM Users
+WHERE mail ~ '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$'
+-- ^ - startsWith;
+-- [] - means that any character that match inner described ranges satisfies this block.
+-- * - zero or more repetitions for the block that goes before
+-- \. - dot is the special symbol that means "any character", so need \ to represent it as symbol in the string.
+-- a-z - allows to check if character matches any character from the range between 2 symbols (a and z in this case).
+-- $ - expression should end with what goes before this symbol.
