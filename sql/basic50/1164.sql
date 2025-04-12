c44b35427e46beee780794d9c002f4743e09fa47
moonjason <jasonmoonjm@gmail.com>
2025-04-12 16:20:11 -0700

    grind

--- sql/basic50/1164.sql
+++ sql/basic50/1164.sql
@@ -16,3 +16,26 @@ WHERE (product_id, change_date) IN (  -- GET THE VALUES OF PRICES THAT EXIST BEF
     WHERE change_date <= '2019-08-16' 
     GROUP BY product_id
 );
+
+
+-- My solution attempted after several days 
+
+SELECT product_id, new_price as price
+FROM Products 
+WHERE (product_id, change_date) IN (
+    SELECT
+        product_id,
+        MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+UNION 
+SELECT product_id, 10 as price
+FROM Products
+WHERE product_id NOT IN (
+    SELECT
+        product_id
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
