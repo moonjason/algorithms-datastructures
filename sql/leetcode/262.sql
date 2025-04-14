b9d01717234e2d8f17fae93a6f73a8ca1cbbb91b
moonjason <jasonmoonjm@gmail.com>
2025-04-13 22:08:52 -0700

    grind

--- sql/leetcode/262.sql
+++ sql/leetcode/262.sql
@@ -32,3 +32,17 @@
 -- The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
 -- Write a solution to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03" with at least one trip. Round Cancellation Rate to two decimal points.
 -- Return the result table in any order.
+
+
+SELECT 
+    request_at as Day,
+    ROUND (COALESCE(COUNT(status) FILTER(WHERE status = 'cancelled_by_client' OR status = 'cancelled_by_driver')::decimal,0) / COUNT(status)::Decimal , 2) as "Cancellation Rate"
+FROM Trips t
+JOIN Users u1
+    ON t.client_id = u1.users_id
+JOIN Users u2
+    ON t.driver_id = u2.users_id
+WHERE u1.banned = 'No'
+    AND u2.banned = 'No'
+    AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY Day
