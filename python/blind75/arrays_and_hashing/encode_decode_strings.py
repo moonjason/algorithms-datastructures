0c84dcff214821b07564e5f1ea739fbc6881d1a8
moonjason <jasonmoonjm@gmail.com>
2025-04-18 11:23:39 -0700

    grind

---
+++ python/blind75/arrays_and_hashing/encode_decode_strings.py
@@ -0,0 +1,22 @@
+# https://neetcode.io/problems/string-encode-and-decode
+
+# Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+# Please implement encode and decode
+
+# Example 1:
+# Input: ["neet","code","love","you"]
+
+# Output:["neet","code","love","you"]
+
+# Example 2:
+# Input: ["we","say",":","yes"]
+
+# Output: ["we","say",":","yes"]
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+    def decode(self, s: str) -> List[str]:
