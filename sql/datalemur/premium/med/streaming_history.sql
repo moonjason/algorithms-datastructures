dcd569b3adbeb0edc7030c7b55af57dbbc0b150e
moonjason <jasonmoonjm@gmail.com>
2025-04-18 18:56:14 -0700

    grind

--- sql/datalemur/premium/med/streaming_history.sql
+++ sql/datalemur/premium/med/streaming_history.sql
@@ -44,3 +44,35 @@ FULL OUTER JOIN songs_history sh
   ON wc.user_id = sh.user_id
     AND wc.song_id = sh.song_id
 ORDER BY song_plays desc
+
+
+
+-- DATA LEMUR SOLUTION USING UNION ALL AND THEN SUMMING 
+
+WITH history AS (
+  SELECT 
+    user_id, 
+    song_id, 
+    song_plays
+  FROM songs_history
+
+  UNION ALL
+
+  SELECT 
+    user_id, 
+    song_id, 
+    COUNT(song_id) AS song_plays
+  FROM songs_weekly
+  WHERE listen_time <= '08/04/2022 23:59:59'
+  GROUP BY user_id, song_id
+)
+
+SELECT 
+  user_id, 
+  song_id, 
+  SUM(song_plays) AS song_count
+FROM history
+GROUP BY 
+  user_id, 
+  song_id
+ORDER BY song_count DESC;
