8edf512780a1d2088da3ae93f4a66e0d8fcfed2e
moonjason <jasonmoonjm@gmail.com>
2025-04-26 11:17:45 -0700

    grind

--- python/blind75/sliding_window/longest_substr_wo_duplicates.py
+++ python/blind75/sliding_window/longest_substr_wo_duplicates.py
@@ -12,21 +12,37 @@ class Solution:
             
             substr = s[i]
             while k < len(s):
-                print(s[k] , substr)
                 if not s[k] in substr:
                     substr = s[i : k + 1]
                     longest = max(longest, len(substr))
                     k += 1 
                 else: 
-                    i = k 
-                    k = i + 1
+                    break
 
             i += 1
-        
+            k = i + 1
+
         return longest
-# PROGRESS ^^ NOT FINISHED 
+# MY TRASH ANSWER ^^^
 
+def lengthOfLongestSubstring(s: str) -> int:
+    seen = set()
+    longest = 0
+    i = 0
+    k = 0
 
+    while k < len(s):
+        if s[k] not in seen:
+            seen.add(s[k])
+            longest = max(longest, k - i + 1)
+            k += 1
+        else:
+            seen.remove(s[i])
+            i += 1
+
+    return longest
+## OPTIMAL SOLUTION USING SETS https://www.w3schools.com/python/python_sets.asp
+## I NEED TO MAKE MY LOOPS BETTER 
 
 # TEST CASE 3 = "dvdf"
 # TEST CASE 3 FAILS BECAUSE I'M ONLY SLIDING THE WINDOW WHEN I SEE A DUPLICATE
